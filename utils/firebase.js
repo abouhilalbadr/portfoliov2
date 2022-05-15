ada030c6624d1b9c9840a7cb67e2ab0705572b61
Abouhilal Badr <abouhilal.badr@gmail.com>
2022-05-15 13:58:47 +0100

    update ENV variables

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyDNkmVd72PD9t8L-ctUvEFap4u3Ap_RM2o",
-  authDomain: "badrabouhilal-47129.firebaseapp.com",
-  projectId: "badrabouhilal-47129",
-  storageBucket: "badrabouhilal-47129.appspot.com",
-  messagingSenderId: "604647803178",
-  appId: "1:604647803178:web:d408629371c8d0af087653",
-  measurementId: "G-G6RW4MNX4T",
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+    measurementId: process.env.MEASUREMENTID,
 };
 
 // Initialize Firebase
